89a8767dd6006f76f9d357b84c90d2de13fdab45
Amani Jammoul <amani.jammoul@mail.mcgill.ca>
2024-03-14 03:05:58 -0400

    task 2 - book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,14 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let isbn = req.params.isbn;
+    let book = books[isbn];
+    res.send(book);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 // Get all books based on title
